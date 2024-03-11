10180adb7ea6513edfa10f5b7d19f5cbf78e8d62
Polina Tyureva <polina.tyureva@devexpress.com>
2024-03-11 16:27:33 +0400

    update example

---
+++ react-report-designer/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useEffect, useRef } from 'react';
+import { DxReportDesigner } from 'devexpress-reporting/dx-reportdesigner';
+import * as ko from 'knockout';
+
+const ReportDesigner = () => {
+  const reportUrl = ko.observable("TestReport");
+  const designerRef = useRef();
+  const requestOptions = {
+    host: "https://localhost:5001/",
+    getDesignerModelAction: "/DXXRD/GetDesignerModel"
+  };
+  useEffect(() => {
+    const designer = new DxReportDesigner(designerRef.current, { reportUrl, requestOptions });
+    designer.render(); 
+    return () => designer.dispose();
+  })
+  return (<div ref={designerRef}></div>);
+}
+
+function App() {
+  return (<div style={{ width: "100%", height: "1000px" }}>
+      <ReportDesigner />
+  </div>);
+  }
+
+export default App;
