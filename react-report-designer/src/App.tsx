484ce7aba53dc427583390382d6c4f8d63b4ec84
pollyndos <56310766+pollyndos@users.noreply.github.com>
2025-01-17 16:27:42 +0400

    Update App.tsx

--- react-report-designer/src/App.tsx
+++ react-report-designer/src/App.tsx
@@ -1,16 +1,21 @@
-import ReportViewer, { RequestOptions } from 'devexpress-reporting-react/dx-report-viewer';
-import 'devextreme/dist/css/dx.light.css';
-import '@devexpress/analytics-core/dist/css/dx-analytics.common.css';
-import '@devexpress/analytics-core/dist/css/dx-analytics.light.css';
-import 'devexpress-reporting/dist/css/dx-webdocumentviewer.css';
+    import ReportDesigner, { RequestOptions} from 'devexpress-reporting-react/dx-report-designer';
+    import 'devextreme/dist/css/dx.light.css';
+    import '@devexpress/analytics-core/dist/css/dx-analytics.common.css';
+    import '@devexpress/analytics-core/dist/css/dx-analytics.light.css';
+    import 'devexpress-reporting/dist/css/dx-webdocumentviewer.css';
+    import 'ace-builds/css/ace.css';  
+    import 'ace-builds/css/theme/dreamweaver.css';  
+    import 'ace-builds/css/theme/ambiance.css';  
+    import '@devexpress/analytics-core/dist/css/dx-querybuilder.css';
+    import 'devexpress-reporting/dist/css/dx-reportdesigner.css';
 
-function App() {
+    function App() {
 
-  return (
-    <ReportViewer reportUrl="TestReport">
-      <RequestOptions host="http://localhost:5000/" invokeAction="DXXRDV" />
-    </ReportViewer>        
-  )
-}
+      return (
+        <ReportDesigner reportUrl="TestReport">
+            <RequestOptions host="http://localhost:5000/" getDesignerModelAction="DXXRD/GetDesignerModel" />
+        </ReportDesigner>       
+      )
+    }
 
-export default App
+    export default App
